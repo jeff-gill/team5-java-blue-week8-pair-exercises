06c49271c2a6dbdd504d7d649b7380fb0ca84a7e
Gordy <gordy.smith1@gmail.com>
2018-07-21 14:11:00 -0400

    Survey update

--- m3-java-capstone/src/main/java/com/techelevator/npgeek/JDBCSurveyDAO.java
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/JDBCSurveyDAO.java
@@ -33,9 +33,9 @@ public class JDBCSurveyDAO implements SurveyDAO{
 	public List<FavoriteParks> getSurveyResults() {
 		
 		List<FavoriteParks> favoritePark = new ArrayList<FavoriteParks>();
-		String sqlSelectSurveyResults = "select parkname, Lower(survey_result.parkcode) as lowerparkcode, count (survey_result.parkcode) as parkcodecount from survey_result" + 
-				"join park on survey_result.parkcode = park.parkcode" + 
-				"group by survey_result.parkcode, park.parkname" + 
+		String sqlSelectSurveyResults = "select parkname, Lower(survey_result.parkcode) as lowerparkcode, count (survey_result.parkcode) as parkcodecount from survey_result " + 
+				"join park on survey_result.parkcode = park.parkcode " + 
+				"group by survey_result.parkcode, park.parkname " + 
 				"order by count (survey_result.parkcode) DESC";
 		
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSelectSurveyResults);
